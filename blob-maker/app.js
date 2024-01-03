7ee4733c6d3f7490f5fc7f758858b7fb71b726fb
qqbn <jakubkroc23@gmail.com>
2024-01-03 23:05:22 +0100

    blob maker new version

--- blob-maker/app.js
+++ blob-maker/app.js
@@ -5,8 +5,10 @@ const leftTop = document.querySelector('#left-top');
 const leftBottom = document.querySelector('#left-bottom');
 const rightTop = document.querySelector('#right-top');
 const rightBottom = document.querySelector('#right-bottom');
-
-
+const inputs = document.querySelectorAll('.range-input');
+const code = document.querySelector('.code');
+const copy = document.querySelector('.copy');
+let styleArr = ['', '', '', '', '/', '', '', '', ''];
 
 height.addEventListener('input', (e) => {
     blob.style.height=e.target.value+'px';
@@ -16,18 +18,23 @@ width.addEventListener('input', (e) => {
     blob.style.width=e.target.value+'px';
 })
 
-leftTop.addEventListener('input', (e) => {
-    blob.style.borderTopLeftRadius=e.target.value+'%';
+inputs.forEach((elem) => {
+    elem.addEventListener('input', (e) => {
+        styleArr.splice(0,1,leftTop.value+'%');
+        styleArr.splice(1,1,(100-leftTop.value)+'%');
+        styleArr.splice(2,1,(100-leftBottom.value)+'%');
+        styleArr.splice(3,1,leftBottom.value+'%');
+        styleArr.splice(6,1,rightTop.value+'%');
+        styleArr.splice(7,1,(100-rightTop.value)+'%');
+        styleArr.splice(5,1,rightBottom.value+'%');
+        styleArr.splice(8,1,(100-rightBottom.value)+'%');
+        const style = styleArr.join(' ');
+        blob.style.borderRadius = style;
+        code.innerHTML = 'border-radius: ' + style+';';
+    })
 })
 
-leftBottom.addEventListener('input', (e) => {
-    blob.style.borderBottomLeftRadius=e.target.value+'%';
-})
-
-rightTop.addEventListener('input', (e) => {
-    blob.style.borderTopRightRadius=e.target.value+'%';
-})
-
-rightBottom.addEventListener('input', (e) => {
-    blob.style.borderBottomRightRadius=e.target.value+'%';
+copy.addEventListener('click', (e) => {
+    navigator.clipboard.writeText(code.innerHTML);
+    alert('code copied');
 })
