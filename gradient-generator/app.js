c410423101bb6f16119e38df24d7298b7d871bb5
qqbn <jakubkroc23@gmail.com>
2024-01-30 23:08:31 +0100

    adding active buttons, gradient string creating

--- gradient-generator/app.js
+++ gradient-generator/app.js
@@ -2,10 +2,27 @@ const leftColorInput = document.querySelector('#left-color');
 const rightColorInput = document.querySelector('#right-color');
 const positionInput = document.querySelector('#position');
 const rotationInput = document.querySelector('#rotation');
+const ranges=document.querySelectorAll('.gradient-range');
 let leftColorVal;
 let rightColorVal;
 let positionVal;
+let rotationVal;
+let leftPositionVal;
+let rightPositionVal;
+let active=0;
 
+
+ranges.forEach(elem=>{
+    elem.addEventListener('mousedown', (e)=>{
+        const found = [...ranges].find(el => el.classList.contains('active'));
+        if(!found){
+            elem.classList.add('active');
+        }else{
+            found.classList.remove('active');
+            elem.classList.add('active');
+        }
+    })
+})
 
 leftColorInput.addEventListener('input', (e)=>{
     leftColorVal=e.target.value;
@@ -24,12 +41,17 @@ positionInput.addEventListener('blur', (e)=>{
 })
 
 rotationInput.addEventListener('input', (e)=>{
-    rotationVal=e.target.value + '&deg;';
+    rotationVal=e.target.value;
 })
 
 rotationInput.addEventListener('blur', (e)=>{
     rotationInput.value=e.target.value + String.fromCharCode(176);
 })
 
+
+const createBackground = () => {
+    console.log(`linear-gradient(${rotationVal}deg, #${leftColorVal} ${leftPositionVal}%, #${rightColorVal} ${rightPositionVal})`);
+    return `linear-gradient(${rotationVal}deg, #${leftColorVal} ${leftPositionVal}%, #${rightColorVal} ${rightPositionVal})`
+}
 
 
