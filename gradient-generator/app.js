e371dda2c15fdef570e675952a3665c0060c874c
qqbn <jakubkroc23@gmail.com>
2024-01-31 21:22:40 +0100

    listeners improve

--- gradient-generator/app.js
+++ gradient-generator/app.js
@@ -3,18 +3,28 @@ const rightColorInput = document.querySelector('#right-color');
 const positionInput = document.querySelector('#position');
 const rotationInput = document.querySelector('#rotation');
 const ranges=document.querySelectorAll('.gradient-range');
-let leftColorVal;
-let rightColorVal;
+let leftColorVal=leftColorInput.value;
+let rightColorVal=rightColorInput.value;
 let positionVal;
-let rotationVal;
-let leftPositionVal;
-let rightPositionVal;
+let rotationVal='100';
+let leftPositionVal='0%';
+let rightPositionVal='100%';
 let active=0;
+const inputs = [leftColorInput, rightColorInput]
 
 
 ranges.forEach(elem=>{
-    elem.addEventListener('mousedown', (e)=>{
+    elem.addEventListener('click', (e)=>{
+        active=Number(elem.getAttribute('data-id'));
         const found = [...ranges].find(el => el.classList.contains('active'));
+        
+        positionInput.value='';
+        if(active){
+            positionInput.value=rightPositionVal;
+        }else{
+            positionInput.value=leftPositionVal;
+        }
+
         if(!found){
             elem.classList.add('active');
         }else{
@@ -22,36 +32,47 @@ ranges.forEach(elem=>{
             elem.classList.add('active');
         }
     })
+
+    elem.addEventListener('mousedown', (e) =>{
+        console.log('mousedown');
+    })
 })
 
-leftColorInput.addEventListener('input', (e)=>{
-    leftColorVal=e.target.value;
-})
-
-rightColorInput.addEventListener('input', (e)=>{
-    rightColorInput=e.target.value;
-})
-
-positionInput.addEventListener('input', (e)=>{
-    positionVal=e.target.value;
+inputs.forEach(elem=>{
+    elem.addEventListener('blur', (e)=>{
+        if(e.target.name.includes('left')){
+            leftColorVal='#'+e.target.value;
+        }else{
+            rightColorVal='#'+e.target.value;
+        }
+        e.target.value='#'+e.target.value;
+        createBackground();
+    })
 })
 
 positionInput.addEventListener('blur', (e)=>{
-    positionInput.value=e.target.value+'%';
+    setPosition(e.target.value+'%');
+    createBackground();
 })
 
-rotationInput.addEventListener('input', (e)=>{
+rotationInput.addEventListener('blur', (e)=>
+{
     rotationVal=e.target.value;
-})
-
-rotationInput.addEventListener('blur', (e)=>{
     rotationInput.value=e.target.value + String.fromCharCode(176);
+    createBackground();
 })
 
 
 const createBackground = () => {
-    console.log(`linear-gradient(${rotationVal}deg, #${leftColorVal} ${leftPositionVal}%, #${rightColorVal} ${rightPositionVal})`);
+    console.log(`linear-gradient(${rotationVal}deg, ${leftColorVal} ${leftPositionVal}, ${rightColorVal} ${rightPositionVal})`);
     return `linear-gradient(${rotationVal}deg, #${leftColorVal} ${leftPositionVal}%, #${rightColorVal} ${rightPositionVal})`
 }
 
-
+const setPosition = (value) => {
+    if(active){
+        rightPositionVal=value;
+    }else{
+        leftPositionVal=value;
+    }
+    positionInput.value=value
+}
